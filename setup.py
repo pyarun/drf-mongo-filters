4a9b65f219405acfa6f23bafc74c71e3f78fb5fc
Arun Mittal <arun.mittal@searshc.com>
2017-09-28 20:38:02 +0530

    updated version of dependencies

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     name="drf-mongo-filters",
     packages=["drf_mongo_filters",],
     version="1.1",
-    install_requires=["Django == 1.7",
-                      "mongoengine >= 0.8, < 0.9",
-                      "djangorestframework >= 3.0, < 3.1"],
+    install_requires=["Django >= 1.11",
+                      "mongoengine >= 0.13",
+                      "djangorestframework >= 3.5.3"],
     # metadata for upload to PyPI
     author="Maxim Vasiliev",
     author_email="qwiglydee@gmail.com",
